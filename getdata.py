af207e478aba26560ac4e54d4e495ccb432b113e
Shish <shish@shishnet.org>
2013-04-06 13:17:04 +0100

    awards layer

--- getdata.py
+++ getdata.py
@@ -1,5 +1,6 @@
 #!./venv/bin/python
 
+# imports {{{
 import requests
 from dogpile.cache import make_region
 from glob import glob
@@ -7,7 +8,8 @@ from collections import defaultdict
 import csv
 import json
 import os
-
+# }}}
+# geolocation {{{
 cache = make_region().configure(
     'dogpile.cache.dbm',
     arguments = {
@@ -30,7 +32,7 @@ def getGeometry(name):
 def getLoc(name):
     args = {
         "format": "json",
-        "q": name,
+        "q": name+", UK",
     }
     return requests.get("http://nominatim.openstreetmap.org/search", params=args).json()
 
@@ -42,85 +44,82 @@ def getCenter(name):
     d = getLoc(name)
     if d:
         return float(d[0]["lat"]), float(d[0]["lon"])
-
-
+# }}}
+# utils {{{
 def tryInt(n):
     try:
         return int(n)
-    except:
-        return -1
+    except Exception:
+        return None
+
 
 def ave(*n):
-    l = [x for x in n if x > 0]
+    l = [x for x in n if x]
     if l:
         return sum(l) / len(l)
 
 
-def genRichList():
+def generic(filepat, parse):
     data = {}
-    for fn in ["./data/TPA/Town-Hall-Rich-List-2012/1.tsv", ]:
+    for fn in glob(filepat):
         for line in csv.reader(file(fn), delimiter="\t"):
             try:
-                region, council, name, job, d2009, d2010 = line[0:6]
-                point = getCenter(council)
-                d2009 = tryInt(d2009)
-                d2010 = tryInt(d2010)
-                da = ave(d2009, d2010)
-                if da and point:
-                    if council in data:
-                        data[council]["val"].append(da)
+                point, value = parse(line)
+                if point and value:
+                    if point in data:
+                        data[point]["val"].append(value)
                     else:
-                        data[council] = {
+                        data[point] = {
                             "lat": point[0],
                             "lon": point[1],
-                            "val": [da, ]
+                            "val": [value, ]
                         }
             except Exception as e:
                 print e
 
     for council in data:
-        data[council]["val"] = ave(*data[council]["val"])
+        if isinstance(data[council]["val"], list):
+            data[council]["val"] = ave(*data[council]["val"])
 
     return data.values()
 
 
-def genRoadSalt():
-    data = {}
-    for fn in ["./data/TPA/Road-Salt-by-council-2009-11/1.tsv", ]:
-        for line in csv.reader(file(fn), delimiter="\t"):
-            try:
-                council, ordered_tonnes_2009 = line[0:2]
-                point = getCenter(council)
-                d2009 = tryInt(ordered_tonnes_2009)
-                da = ave(d2009)
-                if da and point:
-                    if council in data:
-                        data[council]["val"].append(da)
-                    else:
-                        data[council] = {
-                            "lat": point[0],
-                            "lon": point[1],
-                            "val": [da, ]
-                        }
-            except Exception as e:
-                print e
-
-    for council in data:
-        data[council]["val"] = ave(*data[council]["val"])
-
-    return data.values()
-
-
-def dict2jsonp(name, dataFunc):
+def dict2jsonp(name, filepat, parser):
     fn = "layers/%s.js" % name
     if not os.path.exists(fn):
         print "Generating", fn
-        data = dataFunc()
+        data = generic(filepat, parser)
         file(fn, "w").write("addLayer('%s', %s);" % (name, json.dumps(data, indent=4)))
     else:
         print "Already generated", fn
 
+# }}}
 
-dict2jsonp("rich-list", genRichList)
-dict2jsonp("road-salt", genRoadSalt)
+
+def parseRichList(line):
+    region, council, name, job, d2009, d2010 = line[0:6]
+    point = getCenter(council)
+    d2009 = tryInt(d2009)
+    d2010 = tryInt(d2010)
+    return point, ave(d2009, d2010)
+
+dict2jsonp("rich-list", "./data/TPA/Town-Hall-Rich-List-2012/1.tsv", parseRichList)
+
+
+def parseRoadSalt(line):
+    council, ordered_tonnes_2009 = line[0:2]
+    point = getCenter(council)
+    value = tryInt(ordered_tonnes_2009)
+    return point, value
+
+dict2jsonp("road-salt", "./data/TPA/Road-Salt-by-council-2009-11/1.tsv", parseRoadSalt)
+
+
+def parseAwards(line):
+    region, council, name, cost = line[0:4]
+    point = getCenter(council)
+    value = tryInt(cost)
+    return point, value
+
+dict2jsonp("awards", "./data/TPA/Award-Ceremony-Data-2010-2011/1.tsv", parseAwards)
 
